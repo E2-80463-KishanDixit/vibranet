92353e6daff200238d2cca1de894bf8b51adbea1
Kishandixit <kishandixit2000@gmail.com>
2023-05-27 01:43:01 +0700

    setting up the view tamplate engine ejs

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ port = 8000;
 // use express router 
 app.use('/',require('./routes'));
 
+// setting up the view engine
+app.set('view engine','ejs');
+app.set('views','./views');
 
 app.listen(port,(err)=>{
     if(err){
