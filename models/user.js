214b142b881ce3dbda93adcf8841cc7888dd10af
Kishandixit <kishandixit2000@gmail.com>
2023-06-04 18:35:38 +0700

    adding the user schema

---
+++ models/user.js
@@ -0,0 +1,25 @@
+//setting up the user schemma
+const mongoose = require('mongoose');
+
+const userschema = new mongoose.Schema({
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    name:{
+        type:String,
+        required:true
+    }
+},
+{
+    timestamp:true
+})
+
+const User = mongoose.model('User',userschema);
+
+module.exports = User;
