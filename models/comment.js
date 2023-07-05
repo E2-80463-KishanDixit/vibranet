2abe357c333549533498139c27bd1f4d3302dabc
Kishandixit <kishandixit2000@gmail.com>
2023-07-05 17:43:20 +0530

    creating schema for comments

---
+++ models/comment.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+    content:{
+        type:String,
+        required: true
+    },
+    // Comments Belong to a User
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    },
+    post:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Post"
+    }
+},{
+    timeseries:true
+})
+
+const Comment = mongoose.model('Comment',commentSchema);
+
+module.exports = Comment;
--- models/post.js
+++ models/post.js
@@ -9,7 +9,14 @@ const postSchema = new mongoose.Schema({
     user:{
         type: mongoose.Schema.Types.ObjectId,        // user object id
         ref: 'User'                                 // Refer to which schema  
-    }
+    },
+    // Include Array of Ids of all comments in this post schema itself
+    comments:[
+        {
+            type: mongoose.Schema.Types.ObjectId,        // user object id
+            ref: 'Comment'                                 
+        }
+    ]
 },{
     timestamps:true                // it will add "created at and updated add" field in the database
 })
