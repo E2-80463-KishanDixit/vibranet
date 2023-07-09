f30f583fbda234363f7a4ad923889c097aa597b0
Kishandixit <kishandixit2000@gmail.com>
2023-07-09 15:37:04 +0530

    adding delete functionality for post

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,5 +1,5 @@
 const Post = require('../models/post');
-
+const Comment = require('../models/comment');
 module.exports.create = function(req,res){
     Post.create({
         content:req.body.content,
@@ -10,5 +10,22 @@ module.exports.create = function(req,res){
             return;
         }
         return res.redirect('back');
+    });
+}
+
+// TO DELETE  A POST 
+
+module.exports.delete = function(req,res){
+    // First we have to find the post in database
+    Post.findById(req.params.id, function(err,post){
+        // .id means converting the Object id into string
+
+        if(post.user == req.user.id){
+            post.remove();
+
+            Comment.deleteMany({post:req.params.id},function(err){
+                return  res.redirect('back');
+            });
+        }
     });
 }
--- routes/posts.js
+++ routes/posts.js
@@ -5,5 +5,5 @@ const passport = require('passport');
 const postController = require('../controllers/posts_controller');
 
 router.post('/create',passport.checkAuthentication,postController.create);   // passport.checkauthentication will check user who submitted form is authenticated or not 
-
+router.get('/destroy/:id',passport.checkAuthentication,postController.delete)
 module.exports = router;
