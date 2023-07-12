d8475a3586bde55a4984e66e99ac535a5aa48fb7
Kishandixit <kishandixit2000@gmail.com>
2023-07-12 16:03:34 +0530

    creating a post : Receiving Data

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -12,7 +12,8 @@
                 url: '/posts/create',
                 data: newPostForm.serialize(),  // to conver data into JSON format
                 success: function(data){
-                    console.log(data);
+                    let newPost = newPostDom(data.data.post);
+                    $('#posts-list-container>ul').prepend(newPost);
                 },
                 error: function(error){
                     console.log(error.responseText);
@@ -23,6 +24,36 @@
 
 
     // method to create a post in DOM
-    
+    let newPostDom = function(post){
+        return $(`<li id="post-${post._id}">
+        <p>
+            <small>
+              <a class="delete-post-button" href="/posts/destroy/${post.id}">X</a>
+            </small>
+
+          ${post.content}  
+          <br />
+            <small>
+            ${post.user.name}  
+            </small>
+        </p>
+        <div id="post-comments">
+
+                <form action="/comments/create" method="POST">
+                    <input type="text" name="content" placeholder="Type here to add Comment">
+                    <!------ TO IDENTIFY AT WHICH POST USER HAVE COMMENTED --------->
+                    <input type="hidden" name="post" value="${post._id}">
+                    <input type="submit" value="Add Comment">
+                </form>
+
+            <div id="post-comments-list">
+              <ul id="post-comments-${post._id}"></ul>
+            </div>
+        </div>
+        </li>`)
+    }
+
+
+
     createPost();
 }
